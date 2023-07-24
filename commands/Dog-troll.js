dfa07bc2222756eb7f1abc5651d2c0e3e01c3194
Khadher <132037908+Khadher555@users.noreply.github.com>
2023-07-25 01:46:20 +0530

    Create Dog-troll.js

---
+++ commands/Dog-troll.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'askdog',
+    category: 'troll',
+    desc: 'Makes troll image of given text.',
+    async exec(citel, Void,args) {
+      const url = `https://docs-jojo.herokuapp.com/api/meme-gen?top=${args.join(' ').split(";")[0]}&bottom=%20&img=https://i.imgur.com/o07ESQe.jpeg`;
+      citel.imgurl(url,'AI Troll Pack')
+   }
+}
