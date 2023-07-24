0d8ce53615528b66ef3d88ff7ac75e518bd15fcb
Khadher <132037908+Khadher555@users.noreply.github.com>
2023-07-24 16:37:08 +0530

    Create bgm.js

---
+++ commands/bgm.js
@@ -0,0 +1,19 @@
+const { tlang,cmd } = require('../lib')
+const Config = require('../config')
+const axios = require('axios')
+// Put here your url with modified words and urls
+const url = 'https://raw.githubusercontent.com/Khadher555/STUNNING-AI/main/khadher.inc/bgm/bgm.js'
+
+/**
+ cmd({
+            pattern: "bgm",
+            category: "owner",
+            use: '',
+        },
+**/ 
+cmd({ on: "text" }, async (Void,citel,text,{isCreator})=> {
+  let { data } = await axios.get(url)
+  for (vr in data){
+ if(citel.text.toLowerCase().includes(vr)) return Void.sendMessage(citel.chat,{audio: { url : data[vr]},mimetype: 'audio/mpeg'},{quoted:citel})   
+}
+})
