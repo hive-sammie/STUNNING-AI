54ee2e34562f64780cf351444a426a480d0d1a84
Khadher <132037908+Khadher555@users.noreply.github.com>
2023-07-24 23:21:00 +0530

    Support command added

--- commands/group.js
+++ commands/group.js
@@ -81,7 +81,7 @@ cmd({
         citel.reply(`*Check your Pm ${tlang().greet}*`);
         await Void.sendMessage(`${citel.sender}`, {
             image: log0,
-            caption: `*Group Name: Secktor-Support*\n*Group Link:* https://chat.whatsapp.com/Bl2F9UTVU4CBfZU6eVnrbC`,
+            caption: `*Group Name: stunning ai-Support*\n*Group Link:* https://chat.whatsapp.com/HMJQZqjClNgKOitnskMpbJ`,
         });
 
     }
