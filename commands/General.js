ba8565bb34f81bb498c439001fa11ae35c4408fa
Khadher <132037908+Khadher555@users.noreply.github.com>
2023-07-24 19:40:52 +0530

    Update General.js

--- commands/General.js
+++ commands/General.js
@@ -18,7 +18,7 @@ cmd({
         if (!text) return citel.reply(`Hey there! ${citel.pushName}. How are you doing these days?`);
         const { Configuration, OpenAIApi } = require("openai");
         const configuration = new Configuration({
-            apiKey: Config.OPENAI_API_KEY || "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",
+            apiKey: Config.OPENAI_API_KEY || "org-QJjjHR7XhcmGftlLdgEHIoU9",
         });
         const openai = new OpenAIApi(configuration);
         const completion = await openai.createCompletion({
