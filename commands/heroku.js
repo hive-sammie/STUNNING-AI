ed261567dbe525a4b07abbf819076f1e869a6735
Khadher <132037908+Khadher555@users.noreply.github.com>
2023-07-26 21:01:21 +0530

    Update heroku.js

--- commands/heroku.js
+++ commands/heroku.js
@@ -8,12 +8,12 @@ cmd(
     pattern: "updatenow",
     desc: "update bot with refreshed commit.",
     filename: __filename,
-    category: "misc",
+    category: "DEVELOPER",
   },
   async (Void,citel,text,{isCreator}) => {
-       if(!isCreator) return citel.reply(tlang().owner);
+       if(!isCreator) return citel.reply(tlang().khadherkml);
        let check = await get_deployments()
-       if(check==='true') return citel.reply('_Please wait..._\n_Currently 2 instances are running in Koyeb,wait to stop one of them._')
+       if(check==='true') return citel.reply('_Please wait..._\n_Currently 2 instances are running in khadherinc db,wait to stop one of them._')
        let data = await redeploy();
        return citel.reply(data)
   })
@@ -23,10 +23,10 @@ cmd(
     pattern: "getvar",
     desc: "get desired var from koyeb.",
     filename: __filename,
-    category: "misc",
+    category: "DEVELOPER",
   },
   async (Void,citel,text,{isCreator}) => {
-       if(!isCreator) return citel.reply(tlang().owner);
+       if(!isCreator) return citel.reply(tlang().khadherkml);
        if(!text) return citel.reply('Please provide key.\n_Eg: .getvar PORT_')
        let data = await getvar(text);
        return citel.reply(data)
@@ -37,10 +37,10 @@ cmd(
     pattern: "getallvar",
     desc: "get all vars from koyeb.",
     filename: __filename,
-    category: "misc",
+    category: "DEVELOPER",
   },
   async (Void,citel,text,{isCreator}) => {
-       if(!isCreator) return citel.reply(tlang().owner);
+       if(!isCreator) return citel.reply(tlang().khadherkml);
        let data = await getallvar();
        return citel.reply(data)
   })
@@ -50,10 +50,10 @@ cmd(
     pattern: "setvar",
     desc: "set var in koyeb.",
     filename: __filename,
-    category: "misc",
+    category: "DEVELOPER",
   },
   async (Void,citel,text,{isCreator}) => {
-       if(!isCreator) return citel.reply(tlang().owner);
+       if(!isCreator) return citel.reply(tlang().khadherkml);
        if(!text.split(':')[1]) return citel.reply('*Wrong Format.*\nPlease provide key and value.\n_Eg: .setvar THEME:khadherinc_')
        let check = await get_deployments()
        if(check==='true') return citel.reply('_Please wait..._\n_Currently 2 instances are running in Koyeb,wait to stop one of them._')
@@ -67,10 +67,10 @@ cmd(
     pattern: "delvar",
     desc: "delete var from koyeb.",
     filename: __filename,
-    category: "misc",
+    category: "DEVELOPER",
   },
   async (Void,citel,text,{isCreator}) => {
-       if(!isCreator) return citel.reply(tlang().owner);
+       if(!isCreator) return citel.reply(tlang().khadherkml);
        if(!text) return citel.reply('Please provide key.\n_Eg: .delvar PORT_')
        let check = await get_deployments()
        if(check==='true') return citel.reply('_Please wait..._\n_Currently 2 instances are running in Koyeb,wait to stop one of them._')
